8bb3239963b58d3623e5ccf57626cadd1123bcd4
ZhabinYuri <zhabinyuri@gmail.com>
2018-10-01 15:38:46 +0300

    ДЗ №3

--- index.php
+++ index.php
@@ -1,146 +1,107 @@
 <?php
-//Exercise 1
-$a = mt_rand(-100000, 100000);
-$b = mt_rand(-100000, 100000);
+echo "Exercise 1<br>";
 
-echo "$a <br>";
-echo "$b <br>";
-
-if ($a>=0 && $b>=0)
-    echo "Разность чисел $a и $b составляет: " . ($a - $b);
-
-elseif ($a<0 && $b<0)
-    echo "Произведение чисел $a и $b составляет: " . $a * $b;
-    
-else
-    echo "Сумма чисел $a и $b составляет: " . ($a + $b);
-
-echo "<br><br><br>";
-//Exercise 2
-
-$a = rand(0,15);
-echo $a . "<br>";
-
-switch ($a){
-    case 0:
-        echo "<br>" . $a++;
-    case 1:
-        echo "<br>" . $a++;
-    case 2:
-        echo "<br>" . $a++;
-    case 3:
-        echo "<br>" . $a++;
-    case 4:
-        echo "<br>" . $a++;
-    case 5:
-        echo "<br>" . $a++;
-    case 6:
-        echo "<br>" . $a++;
-    case 7:
-        echo "<br>" . $a++;
-    case 8:
-        echo "<br>" . $a++;
-    case 9:
-        echo "<br>" . $a++;
-    case 10:
-        echo "<br>" . $a++;
-    case 11:
-        echo "<br>" . $a++;
-    case 12:
-        echo "<br>" . $a++;
-    case 13:
-        echo "<br>" . $a++;
-    case 14:
-        echo "<br>" . $a++;
-    case 15:
-        echo "<br>" . $a;         
+$i = 0;
+while ($i <= 100){
+    if ($i % 3 == 0){
+        echo $i . " ";
+    }
+    $i++;
 }
 
-echo "<br><br><br>";
-//Exercise 3
+echo "<br><br>Exercise 2<br>";
 
-function sum($arg1, $arg2){
-    return $arg1 + $arg2;
-}
-
-function dif($arg1, $arg2){
-    return $arg1 - $arg2;
-}
-
-function mult($arg1, $arg2){
-    return $arg1 * $arg2;
-}
-
-function div($arg1, $arg2){
-    if ($arg2 !=0){
-       return $arg1 / $arg2; 
+$i = 0;
+do {
+    if ($i == 0){
+        echo "$i - это ноль.<br>";
+    }
+    elseif ($i % 2 == 0){
+        echo "$i - это чётное число.<br>";
     }
     else {
-        echo ("На ноль делить нельзя.");
+        echo "$i - это нечётное число.<br>";
+    }
+    $i++;
+}
+while ($i <= 10);
+
+echo "<br><br>Exercise 3<br>";
+
+$cities = [
+    "Московская область" => ["Москва", "Зеленоград", "Клин"], "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"], "Тульская область" => ["Кимовск", "Донской", "Узловая", "Новомосковск"]
+    ];
+foreach ($cities as $key => $value){
+    $value = implode(", ", $value);
+    echo "$key: <br> $value <br>";
+}
+
+echo "<br><br>Exercise 4<br>";
+
+$abc = ["а"=>"a", "б"=>"b", "в"=>"v", "г"=>"g", "д"=>"d", "е"=>"e", "ж"=>"zh", "з"=>"z", "и"=>"i", "й"=>"j", "к"=>"k", "л"=>"l", "м"=>"m", "н"=>"n", "о"=>"o", "п"=>"p", "р"=>"r", "с"=>"s", "т"=>"t", "у"=>"u", "ф"=>"f", "х"=>"kh", "ц"=>"ts", "ч"=>"ch", "ш"=>"sh", "щ"=>"shch", "ъ"=>"'", "ы"=>"y", "ь"=>"'", "э"=>"e", "ю"=>"yu", "я"=>"ya"];
+
+strtr($sentence, $abc); //Функция транслитерации,совпадающие ключи заменяются их значениями
+echo $perevod = strtr("портал обучения", $abc);
+echo "<br>";
+
+array_flip($abc); //Меняет местами ключи и их значения
+echo $perevod = strtr("programmirovaniyu", array_flip($abc)); // Обратная транслитерация
+
+echo "<br><br>Exercise 5<br>";
+
+function changeSpaces($string){
+    echo str_replace(" ", "_", $string);
+}
+echo changeSpaces("груша яблоко банан");
+
+
+echo "<br><br>Exercise 6<br>";
+
+$startpage = ["GeekUniversity", "Курсы" => ["PHP", "Python", "Ruby"], "Форум"];
+
+function Menu($array){
+    $MenuArray[] = '<ul>';
+    foreach ($array as $key => $value){
+        if (is_array($value)) {
+            $MenuArray[] = '<li>' . $key . Menu($value) . '</li>';
+        } else {
+            $MenuArray[] = '<li>' . $value . '</li>';
+        }
+    }
+    $MenuArray[] = '</ul>';
+    return implode($MenuArray);
+}
+echo Menu($startpage);
+
+
+echo "<br><br>Exercise 7<br>";
+
+for($i = 0; $i < 10; print($i++) . " "){
+    // пусто
+}
+
+echo "<br><br>Exercise 8<br>";
+
+$cities = [
+    "Московская область" => ["Москва", "Зеленоград", "Клин"], "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"], "Тульская область" => ["Кимовск", "Донской", "Узловая", "Новомосковск"]
+    ];
+foreach ($cities as $key => $value){
+    echo "$key<br>";
+    foreach ($value as $key => $value){
+        if (mb_substr($value, 0, 2) == 'К'){
+            echo "$value<br>";
+        }
     }
 }
-//Exerise 4
 
-function mathOperation($arg1, $arg2, $operation){
-    switch ($operation){
-        case 'addition':
-            echo sum($arg1, $arg2);
-            break;
-        case 'subtraction':
-            echo dif($arg1, $arg2);
-            break;
-        case 'multiplication':
-            echo mult($arg1, $arg2);
-            break;
-        case 'division':
-            echo div($arg1, $arg2);
-            break;
-        default:
-            echo 'Неправильное название оперции.';
-            break;
-    }
-}
+echo "<br><br>Exercise 9<br>";
 
-echo mathOperation(7, 0, division);
+function translite ($sentence){
+    $abc = ["а"=>"a", "б"=>"b", "в"=>"v", "г"=>"g", "д"=>"d", "е"=>"e", "ж"=>"zh", "з"=>"z", "и"=>"i", "й"=>"j", "к"=>"k", "л"=>"l", "м"=>"m", "н"=>"n", "о"=>"o", "п"=>"p", "р"=>"r", "с"=>"s", "т"=>"t", "у"=>"u", "ф"=>"f", "х"=>"kh", "ц"=>"ts", "ч"=>"ch", "ш"=>"sh", "щ"=>"shch", "ъ"=>"'", "ы"=>"y", "ь"=>"'", "э"=>"e", "ю"=>"yu", "я"=>"ya"];
+    $newstring = strtr($sentence, $abc);
+    echo changeSpaces($newstring);
+}
+translite("совпадающие ключи заменяются их значениями");
 
-echo "<br><br><br>";
-//Exercise 6
-
-function power($val, $pow){
-    if ($pow == 0){
-        return 1;
-    }
-    else {
-        return ($val * power($val, $pow-1)); // Когда $pow достигнет 1, сработает первый if
-    }   
-}
-echo power(5, 3);
-
-echo "<br><br><br>";
-//Exercise 7
-
-$hours = date('H');
-$minutes = date('i');
-
-if ($hours >= 5 && $hours <=20){
-    $hoursEnding = "часов";
-}
-elseif ($hours == 1 || $hours ==21){
-    $hoursEnding = "час";
-}
-else {
-    $hoursEnding = "часа";
-}
-if (($minutes >= 05 && $minutes <= 20) || ($minutes >= 25 && $minutes <= 30 ) || ($minutes >= 35 && $minutes <= 40) || ($minutes >= 45 && $minutes <= 50) || ($minutes >= 55 && $minutes <=60)){
-    $minutesEnding = "минут";
-}
-elseif ($minutes == 01 || $minutes == 21 || $minutes == 31 || $minutes == 41 || $minutes == 51){
-    $minutesEnding = "минута";
-}
-else {
-    $minutesEnding = "минуты";
-}
-
-echo $hours . ' ' . $hoursEnding . ' ' . $minutes . ' ' . $minutesEnding;
 
 ?>
