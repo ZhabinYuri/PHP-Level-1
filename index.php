d757d5d2f10154cdfa950a086d0ce9fd62c8c4e2
ZhabinYuri <zhabinyuri@gmail.com>
2018-10-04 12:48:16 +0300

    ДЗ №4.

--- index.php
+++ index.php
@@ -1,107 +1,45 @@
 <?php
-echo "Exercise 1<br>";
-
-$i = 0;
-while ($i <= 100){
-    if ($i % 3 == 0){
-        echo $i . " ";
-    }
-    $i++;
-}
-
-echo "<br><br>Exercise 2<br>";
-
-$i = 0;
-do {
-    if ($i == 0){
-        echo "$i - это ноль.<br>";
-    }
-    elseif ($i % 2 == 0){
-        echo "$i - это чётное число.<br>";
-    }
-    else {
-        echo "$i - это нечётное число.<br>";
-    }
-    $i++;
-}
-while ($i <= 10);
-
-echo "<br><br>Exercise 3<br>";
-
-$cities = [
-    "Московская область" => ["Москва", "Зеленоград", "Клин"], "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"], "Тульская область" => ["Кимовск", "Донской", "Узловая", "Новомосковск"]
-    ];
-foreach ($cities as $key => $value){
-    $value = implode(", ", $value);
-    echo "$key: <br> $value <br>";
-}
-
-echo "<br><br>Exercise 4<br>";
-
-$abc = ["а"=>"a", "б"=>"b", "в"=>"v", "г"=>"g", "д"=>"d", "е"=>"e", "ж"=>"zh", "з"=>"z", "и"=>"i", "й"=>"j", "к"=>"k", "л"=>"l", "м"=>"m", "н"=>"n", "о"=>"o", "п"=>"p", "р"=>"r", "с"=>"s", "т"=>"t", "у"=>"u", "ф"=>"f", "х"=>"kh", "ц"=>"ts", "ч"=>"ch", "ш"=>"sh", "щ"=>"shch", "ъ"=>"'", "ы"=>"y", "ь"=>"'", "э"=>"e", "ю"=>"yu", "я"=>"ya"];
-
-strtr($sentence, $abc); //Функция транслитерации,совпадающие ключи заменяются их значениями
-echo $perevod = strtr("портал обучения", $abc);
-echo "<br>";
-
-array_flip($abc); //Меняет местами ключи и их значения
-echo $perevod = strtr("programmirovaniyu", array_flip($abc)); // Обратная транслитерация
-
-echo "<br><br>Exercise 5<br>";
-
-function changeSpaces($string){
-    echo str_replace(" ", "_", $string);
-}
-echo changeSpaces("груша яблоко банан");
-
-
-echo "<br><br>Exercise 6<br>";
-
-$startpage = ["GeekUniversity", "Курсы" => ["PHP", "Python", "Ruby"], "Форум"];
-
-function Menu($array){
-    $MenuArray[] = '<ul>';
-    foreach ($array as $key => $value){
-        if (is_array($value)) {
-            $MenuArray[] = '<li>' . $key . Menu($value) . '</li>';
-        } else {
-            $MenuArray[] = '<li>' . $value . '</li>';
-        }
-    }
-    $MenuArray[] = '</ul>';
-    return implode($MenuArray);
-}
-echo Menu($startpage);
-
-
-echo "<br><br>Exercise 7<br>";
-
-for($i = 0; $i < 10; print($i++) . " "){
-    // пусто
-}
-
-echo "<br><br>Exercise 8<br>";
-
-$cities = [
-    "Московская область" => ["Москва", "Зеленоград", "Клин"], "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"], "Тульская область" => ["Кимовск", "Донской", "Узловая", "Новомосковск"]
-    ];
-foreach ($cities as $key => $value){
-    echo "$key<br>";
-    foreach ($value as $key => $value){
-        if (mb_substr($value, 0, 2) == 'К'){
-            echo "$value<br>";
-        }
+require 'config.php'; //Подключаем файл конфигурации
+require 'process.php'; //Подключаем файл-обработчик
+//Форма отправки изображений на сжатие
+if(isset($_FILES['fupload'])) {
+if(preg_match('/[.](jpg)|(gif)|(png)$/', $_FILES['fupload']['name'])) { //Допустимые форматы изображений для загрузки
+    $filename = $_FILES['fupload']['name'];
+    $source = $_FILES['fupload']['tmp_name']; 
+    $target = $path_to_image_directory . $filename;
+    move_uploaded_file($source, $target);
+    createThumbnail($filename);
     }
 }
 
-echo "<br><br>Exercise 9<br>";
-
-function translite ($sentence){
-    $abc = ["а"=>"a", "б"=>"b", "в"=>"v", "г"=>"g", "д"=>"d", "е"=>"e", "ж"=>"zh", "з"=>"z", "и"=>"i", "й"=>"j", "к"=>"k", "л"=>"l", "м"=>"m", "н"=>"n", "о"=>"o", "п"=>"p", "р"=>"r", "с"=>"s", "т"=>"t", "у"=>"u", "ф"=>"f", "х"=>"kh", "ц"=>"ts", "ч"=>"ch", "ш"=>"sh", "щ"=>"shch", "ъ"=>"'", "ы"=>"y", "ь"=>"'", "э"=>"e", "ю"=>"yu", "я"=>"ya"];
-    $newstring = strtr($sentence, $abc);
-    echo changeSpaces($newstring);
-}
-translite("совпадающие ключи заменяются их значениями");
-
-
+//Скрипт вывода картинок 
+$dir_small = 'small/'; //Папка с миниатюрами
+$dir_big = 'big/'; //Папка с полноразмерными картинками
+$cols = 4; //Количество столбцов в будущей таблице с картинками
+$files = scandir($dir_small); //Сканируем всё содержимое папки с миниатюрами
+echo "<table>";
+$k = 0; //Вспомогательный счётчик для перехода на новые строки
+for ($i = 0; $i < count($files); $i++) {
+    if (($files[$i] != ".") && ($files[$i] != "..")) { //Исключаем текущий и родительский  каталоги
+      if ($k % $cols == 0) {
+          echo "<tr>"; } //Добавляем новую строку
+    echo "<td>";
+    $path_to_small = $dir_small.$files[$i]; //Путь к миниатюре
+    $path_to_big = $dir_big.$files[$i]; //Путь к картинке
+    echo "<a href='$path_to_big' target='_blank'>"; //Cсылка на картинку
+    echo "<img src='$path_to_small' alt='' />"; //Вывод миниатюр
+    echo "</a>";
+    echo "</td>";
+    if ((($k + 1) % $cols == 0) || (($i + 1) == count($files))) {
+        echo "</tr>"; }
+    $k++; //Увеличиваем вспомогательный счётчик
+    }
+  }
+echo "</table>";
 ?>
+
+<h1>Загрузка изображений:</h1>	
+<form enctype="multipart/form-data" action="" method="post">		
+<input type="file" name="fupload" />		
+<input type="submit" value="Загрузить" />
+</form>
